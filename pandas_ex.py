0184c108e17a7b1b572ad19547903fc272b82890
praful-coder <prafulkardewar@gmail.com>
2021-01-26 18:34:14 +0530

    newfile added

---
+++ pandas_ex.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+import pandas as pd
+dict = {'id':[1,2,3],'name':['praful','ravi','raju'],'age':[12,34,56]}
+
+df = pd.read_csv(r"C:\Users\lenovo\Desktop\Sample-Spreadsheet-10-rows.csv")
+print((df[3]))
